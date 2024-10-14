4ccf8e077fee92ada6826964ae6c0ce4f8cf3eba
Nikita <perfect1201@ukr.net>
2024-10-15 01:16:47 +0300

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 import { headerFont } from "./ui/fonts";
-import AppFooter from "./ui/footer";
+// import AppFooter from "./ui/footer";
 import AppHeader from "./ui/AppHeader/header";
-import Image from "next/image";
-// import tvIcon from "./ui/TV-icon.svg";
 import TVIcon from "./ui/TV-icon.svg";
 
 export default function Home() {
   return (
     <div className="max-w-screen-2xl mx-auto w-screen mt-6 mb-6 mr-8 ml-8 bg-orange-50 rounded-xl pl-6 pr-6 pb-4 box-border overflow-y-scroll hide-scrollbar">
       <AppHeader />
-      <main className="flex">
-        <div className="flex flex-1 bg-orange-300 pl-6 pt-4 mt-4 rounded-2xl relative">
-          <div className="pb-4">
+      <main className="flex mt-4">
+        <div className="flex flex-1 flex-col items-center sm:flex-row sm:items-end">
+          <div className="bg-orange-300 min-h-64 pl-6 pr-6 pb-4 pt-4 rounded-2xl box-content">
             <h1 className={`${headerFont.className} text-5xl`}>
               Welcome to Rick & Morty fanlib page!
             </h1>
@@ -26,7 +24,7 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex p-4 w-fit h-fit rounded-2xl rounded-r-none bg-orange-50">
+          <div className="flex pl-4 pr-4 pt-4 w-fit h-fit rounded-2xl bg-orange-50">
             <TVIcon className="w-56 h-56" />
           </div>
         </div>
